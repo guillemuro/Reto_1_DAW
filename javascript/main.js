42ce9f9c4f733a3893930f9566cdc53c6d8ddbcc
paula_mb <a26619@svalero.com>
2023-05-09 05:56:21 +0200

    Plantilla de la página de merchandising

--- javascript/main.js
+++ javascript/main.js
@@ -33,3 +33,68 @@ btnCerrarPopup.addEventListener('click', function(e){
 	overlay.classList.remove('active');
 	popup.classList.remove('active');
 });
+
+
+
+/*PAGINA MERCHANDISING*/
+var abrirInfoProducto1 = document.getElementById('producto_1'),
+	abrirInfoProducto2 = document.getElementById('producto_2'),
+	abrirInfoProducto3 = document.getElementById('producto_3'),
+	abrirInfoProducto4 = document.getElementById('producto_4'),
+	producto1 = document.getElementById('producto_1_popup'),
+	producto2 = document.getElementById('producto_2_popup'),
+	producto3 = document.getElementById('producto_3_popup'),
+	producto4 = document.getElementById('producto_4_popup');
+
+abrirInfoProducto1.addEventListener('click', function(){
+	producto1.classList.add('active');
+});
+abrirInfoProducto2.addEventListener('click', function(){
+	producto2.classList.add('active');
+});
+abrirInfoProducto3.addEventListener('click', function(){
+	producto3.classList.add('active');
+});
+abrirInfoProducto4.addEventListener('click', function(){
+	producto4.classList.add('active');
+});
+
+
+var btnCerrarPopup = document.getElementById('x_zona_privada_1');
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	producto1.classList.remove('active');
+});
+var btnCerrarPopup1 = document.getElementById('x_zona_privada_2');
+btnCerrarPopup1.addEventListener('click', function(e){
+	e.preventDefault();
+	producto2.classList.remove('active');
+});
+var btnCerrarPopup2 = document.getElementById('x_zona_privada_3');
+btnCerrarPopup2.addEventListener('click', function(e){
+	e.preventDefault();
+	producto3.classList.remove('active');
+});
+var btnCerrarPopup3 = document.getElementById('x_zona_privada_4');
+btnCerrarPopup3.addEventListener('click', function(e){
+	e.preventDefault();
+	producto4.classList.remove('active');
+});
+
+
+let i = 1;
+var restar = document.getElementById('resta'),
+    sumar = document.getElementById('suma'),
+    numActual = document.getElementById('numero');
+restar.addEventListener('click', function(){
+    if (i > 1) {
+        i--;
+        numActual.innerText = i;
+        console.log(i);
+    }
+});
+sumar.addEventListener('click', function(){
+    i++;
+    numActual.innerText = i;
+    console.log(i);
+});
