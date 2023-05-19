cd5a89572db4bfd310b06529fa50118202a5217d
guillemuro <villuro9798@gmail.com>
2023-05-19 22:34:27 +0200

    Ajustes del localStorage

--- src/main/webapp/script/main.js
+++ src/main/webapp/script/main.js
@@ -1,5 +1,5 @@
 function devolverValor() {
-    let url = "http://localhost:8080/src/Controller?ACTION=USER.FIND"
+    let url = "http://localhost:8080/src/Controller?ACTION=USER.FIND"/*DEBERIA IR EN EL PUERTO 8080?!*/
     fetch(url)
         .then(response => response.json())
         .then(json => {
@@ -9,3 +9,6 @@ function devolverValor() {
             console.error(error)
         });
 }
+
+/*B2B DEBE CORRER POR UN PUERTO DIFERENTE AL 8080 (8081)
+Y LA PAGINA WEB DEBE CORRER POR EL PUERTO 80 Y 8080*/
