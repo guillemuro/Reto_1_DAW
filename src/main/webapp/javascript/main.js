09c63967b5836874c1a825da3f18be3cc0fe67bb
guillemuro <villuro9798@gmail.com>
2023-05-22 12:02:00 +0200

    El Merchan lo pilla la bbdd

--- src/main/webapp/javascript/main.js
+++ src/main/webapp/javascript/main.js
@@ -1,4 +1,6 @@
 window.onload = function () {
+
+    /*LOS CAFES SON RECOGIDOS EN LA BBDD*/
     fetch('http://localhost:8081/src/Controller?ACTION=PRODUCT.FIND&TYPE=1').then(function (response) {
         response.json().then(function (json) {
             let cuerpoMenu = document.getElementById("classMenu")
@@ -36,6 +38,54 @@ window.onload = function () {
                                 d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path></svg>
                     </button>
                 <div>
+                `
+                cuerpoMenu.appendChild(div);
+            });
+        });
+    })
+        .catch(function (error) {
+            console.log('Hubo un problema con la petición Fetch:' + error.message);
+        });
+
+
+
+
+
+    fetch('http://localhost:8081/src/Controller?ACTION=PRODUCT.FIND&TYPE=6').then(function (response) {
+        response.json().then(function (json) {
+            let cuerpoMenu = document.getElementById("merchan")
+            cuerpoMenu.innerHTML = ""
+            json.forEach(element => {
+                let div = document.createElement('div')
+                div.innerHTML = `
+                <div class="producto_cafe">
+                        <div class="informacion_producto_vendido">
+                        <h4>${element.productDesc}</h4>
+                            <p class="informacion_prod_vend_texto">
+                            ${element.productTitle}
+                            </p>
+                            <div class="informacion_producto_vendido_flex">
+                                <a class="enlace_producto"
+                                    href="../imagenes/tshirt_medidas.jpg"
+                                    target="blank" alt="t-shirt"><p
+                                        class="botones_info">Size</p></a>
+                            </div>
+                        </div>
+                        <div class="elemento_ocultado_posteriormente">
+                            <img class="cafe_menu"
+                                src="${element.productImg}" />
+                            <div class="info_nombre_precio">
+                                <p><strong>${element.productName}</strong></p>
+                                <p>$ ${element.productPrice}0</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="boton_carrito" onclick="addProduct(${element.productId})">
+                        <svg viewBox="0 0 16 16" class="carrito_boton"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"><path
+                                d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path></svg>
+                    </button>
                 `
                 cuerpoMenu.appendChild(div);
             });
