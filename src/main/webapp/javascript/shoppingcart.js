07cd0da0eb053f889f3e46aa7d91d76a78dbba12
Paula Mateo <a26619@svalero.com>
2023-05-29 14:31:41 +0200

    JavaScript del carrito

--- src/main/webapp/javascript/shoppingcart.js
+++ src/main/webapp/javascript/shoppingcart.js
@@ -3,11 +3,12 @@
 
 
 let contadorIcono = document.querySelector('.contador_productos');
-let menuMerchandising = document.querySelector('.cuerpo_menu');
+let menu = document.querySelector('.cuerpo_menu');
 let contador = sessionStorage.getItem('contador') || 0;
 
+
 //INCREMENTAR CONTADOR AL DARLE CLIC AL BOTON AÑADIR AL CARRITO
-menuMerchandising.addEventListener('click', function (event) {
+menu.addEventListener('click', function (event) {
     if (event.target.classList.contains('boton_carrito') || event.target.closest('.boton_carrito')) {
         contador++;
         contadorIcono.textContent = contador.toString();
@@ -16,6 +17,136 @@ menuMerchandising.addEventListener('click', function (event) {
 });
 
 contadorIcono.textContent = contador.toString();
+
+
+
+// DARLE AL BOTÓN (REGISTRO DE A QUE PRODUCTOS LES DAS CLIC, Y LO MANDA AL CARRITO)
+var botonesCarrito = document.getElementsByClassName('boton_carrito');
+
+for (var i = 0; i < botonesCarrito.length; i++) {
+  botonesCarrito[i].addEventListener('click', function() {
+    var producto = this.parentNode.getElementsByClassName('info_nombre_precio')[0];
+    var nombre = producto.getElementsByClassName('nombre_producto')[0].textContent;
+    var precio = producto.getElementsByClassName('precio_producto')[0].textContent;
+
+    // Obtener los productos del carrito almacenados en el localStorage
+    var productosEnCarrito = localStorage.getItem('productosEnCarrito');
+
+    if (productosEnCarrito) {
+      // Si hay productos en el carrito, se convierte la cadena en un objeto
+      productosEnCarrito = JSON.parse(productosEnCarrito);
+    } else {
+      // Si no hay productos en el carrito, se inicializa como un objeto vacío
+      productosEnCarrito = {};
+    }
+
+    // Verificar si el producto ya está en el carrito
+    if (productosEnCarrito[nombre]) {
+      // Si el producto ya está en el carrito, se incrementa la cantidad
+      productosEnCarrito[nombre].cantidad++;
+    } else {
+      // Si el producto no está en el carrito, se agrega con una cantidad inicial de 1
+      productosEnCarrito[nombre] = {
+        nombre: nombre,
+        precio: precio,
+        cantidad: 1
+      };
+    }
+
+    // Guardar los productos actualizados en el carrito en el localStorage
+    localStorage.setItem('productosEnCarrito', JSON.stringify(productosEnCarrito));
+  });
+}
+
+
+
+// DESPLEGABLE (CARRITO)
+document.addEventListener('DOMContentLoaded', function() {
+    var carritoElement = document.getElementById('carrito');
+    var subtotalElement = document.querySelector('.subtotal');
+    var shippingCostsElement = document.querySelector('.shippingcosts');
+    var totalElement = document.querySelector('.total_precio');
+  
+    var subtotal = 0;
+  
+    // Obtener los productos del carrito almacenados en el localStorage
+    var productosEnCarrito = localStorage.getItem('productosEnCarrito');
+  
+    if (productosEnCarrito) {
+      // Si hay productos en el carrito, se convierte la cadena en un objeto
+      productosEnCarrito = JSON.parse(productosEnCarrito);
+  
+      // Recorrer los productos en el carrito y mostrar su información
+      for (var nombreProducto in productosEnCarrito) {
+        var producto = productosEnCarrito[nombreProducto];
+        var elementoProducto = document.createElement('div');
+        elementoProducto.classList.add('producto_agregado_carrito');
+  
+        var imgCarrito = document.createElement('img');
+        imgCarrito.classList.add('img_carrito');
+        imgCarrito.src = '../imagenes/imagen_prueba.png'; /*CAMBIAR*/
+        imgCarrito.alt = 'carrito';
+        elementoProducto.appendChild(imgCarrito);
+  
+        var informacionProductoCarrito = document.createElement('div');
+        informacionProductoCarrito.classList.add('informacion_producto_carrito');
+        elementoProducto.appendChild(informacionProductoCarrito);
+  
+        var h2NombreProducto = document.createElement('h2');
+        h2NombreProducto.textContent = producto.nombre;
+        informacionProductoCarrito.appendChild(h2NombreProducto);
+  
+        var divFlex = document.createElement('div');
+        divFlex.classList.add('flex');
+        informacionProductoCarrito.appendChild(divFlex);
+  
+        var divQuantity = document.createElement('div');
+        divQuantity.classList.add('quantity');
+        divFlex.appendChild(divQuantity);
+  
+        var pQuantity = document.createElement('p');
+        pQuantity.textContent = 'Quantity ';
+        divQuantity.appendChild(pQuantity);
+  
+        var divQuantityBoton = document.createElement('div');
+        divQuantityBoton.classList.add('quantity_boton');
+        divQuantity.appendChild(divQuantityBoton);
+  
+        var spanNumero = document.createElement('span');
+        spanNumero.classList.add('numero');
+        spanNumero.innerHTML = '<strong>' + producto.cantidad + '</strong>';
+        divQuantityBoton.appendChild(spanNumero);
+  
+        var h3Precio = document.createElement('p'); /*OJO*/
+        h3Precio.textContent = '$ ' + producto.precio + '/ unit';
+        divFlex.appendChild(h3Precio);
+  
+        var precioTotalProductos = document.createElement('h3');
+        precioTotalProductos.textContent = '$ ' + (parseFloat(producto.precio) * producto.cantidad).toFixed(2);
+        divFlex.appendChild(precioTotalProductos);
+        
+  
+        var precioFinal = parseFloat(producto.precio) * producto.cantidad; // Cálculo del precio final
+        subtotal += precioFinal; // Sumar al subtotal
+  
+        carritoElement.appendChild(elementoProducto);
+      }
+    } else {
+        // Si no hay productos en el carrito, se muestra un mensaje indicando que está vacío
+        carritoElement.textContent = 'The shopping cart is empty!';
+    }
+    // Mostrar los valores iniciales de las variables en el HTML
+    subtotalElement.textContent = '$ ' + subtotal.toFixed(2);
+    shippingCostsElement.textContent = '$ ' + '2.50';
+    totalElement.textContent = '$ ' + (subtotal + 2.50).toFixed(2);
+  });
+
+
+
+
+
+
+
 
 
 
